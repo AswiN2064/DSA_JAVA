b2fce5b0f0a788f0121c6de7e4dbe90f111996ed
AswiN2064 <nagamurali2064@gmail.com>
2024-06-10 16:34:08 +0530

    added 2-d array

---
+++ Arrays/twoDimArrays.java
@@ -0,0 +1,32 @@
+package Arrays;
+import java.util.Scanner;
+
+public class twoDimArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Rows: ");
+        int row = sc.nextInt();
+        System.out.println("Enter the number of Cols: ");
+        int col = sc.nextInt();
+        int[][] arr = new int[row][col];
+        System.out.println();
+
+        // Setting 2d array
+        System.out.println("Enter the Elements of 2-d Array");
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println();
+
+        // Traversal
+        System.out.println("Traversing an 2d array:");
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
