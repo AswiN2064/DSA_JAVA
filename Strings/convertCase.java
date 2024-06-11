afa56d3e1ec8a4230034e0fc748c463bac735c3f
AswiN2064 <nagamurali2064@gmail.com>
2024-06-11 14:46:22 +0530

    Added converting upper case to lower case

---
+++ Strings/convertCase.java
@@ -0,0 +1,26 @@
+package Strings;
+import java.util.*;
+public class convertCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Sentence: ");
+        String str = sc.nextLine();
+        upperToLower(str);
+    }
+
+    public static void upperToLower(String str) {
+        char[] charr = str.toCharArray();
+
+        for(int i = 0; i < charr.length; i++){
+            char ch = charr[i];
+            if(ch>='A' && ch <='Z') {
+                char ansch = (char)(ch - 'A' + 'a');
+                charr[i] = ansch;
+            }
+        }
+
+        for(int i = 0; i < charr.length; i++){
+            System.out.print(charr[i]);
+        }
+    }
+}
