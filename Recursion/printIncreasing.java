4fe7d57146772496bd80346f6e81cef3715199f6
AswiN2064 <nagamurali2064@gmail.com>
2024-06-13 12:44:51 +0530

    added recursion based printing order

---
+++ Recursion/printIncreasing.java
@@ -0,0 +1,19 @@
+package Recursion;
+import java.util.*;
+
+public class printIncreasing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        increasingOrder(n);
+    }
+    static void increasingOrder(int n){
+        if(n == 0){
+            return;
+        }
+        increasingOrder(n-1);
+        System.out.print(n+" ");
+        
+    }
+}
