3bb6886b4c21c0b9bc30de2d36b566316b299519
AswiN2064 <nagamurali2064@gmail.com>
2024-06-17 08:04:24 +0530

    Added striding in arrays

--- Arrays/striding.java
+++ Arrays/striding.java
@@ -4,7 +4,21 @@ import java.util.*;
 public class striding {
     public static void main(String[] args) {
         int arr[] = {1,5,3,4,5,6,7,8};
-        int stride = 3;
+        Scanner sc = new Scanner(System.in);
+
+        // reading input for n =  array size;
+        System.out.println("Enter the Number of Elements of the array: ");
+        int n = sc.nextInt();
+
+        // reading input for array;
+        System.out.println("Enter the Elements of the array: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // reading Stride input;
+        System.out.println("Enter the value of Stride: ");
+        int stride = sc.nextInt();
 
         for(int i = 0; i <= arr.length - stride; i++){
             System.out.println("Interation no :" + (i + 1));
