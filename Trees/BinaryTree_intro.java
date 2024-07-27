1e5511f6d1f8b822218520f6d706d0eefbd00b24
AswiN2064 <nagamurali2064@gmail.com>
2024-07-27 13:12:18 +0530

    Added introduction to tree Data Structure

---
+++ Trees/BinaryTree_intro.java
@@ -0,0 +1,18 @@
+package Trees;
+
+public class BinaryTree_intro {
+
+    class Node{
+        int data;
+        Node left;
+        Node right;
+    }
+
+    public static void main(String[] args) {
+        Node tree = new Node();
+        tree.data = 10;
+        tree.left = null;
+        tree.right = null;
+    }
+    
+}
